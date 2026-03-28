2b491629b164b581bf6c093614e887ff63e85c87
Borys35 <boryskac10@gmail.com>
2026-03-28 12:52:14 +0100

    add new navbar

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import { Navbar } from "./components/navbar/navbar";
+import { Navbar } from "./components/navbar";
 import { ThemeProvider } from "./components/theme-provider";
 
 export const links: Route.LinksFunction = () => [
